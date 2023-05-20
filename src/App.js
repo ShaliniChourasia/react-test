9183979e88de4f5ea25548aa764333dc2df48563
Luke Hayes <lukehayesa@gmail.com>
2023-05-19 20:57:17 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing
+          Testing1
         </p>
         <p>
-          Testing 2
-        </p>
+          Testing2
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
